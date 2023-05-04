c98d3ddc38969c54d2a5396acaaea8dcf5b3f6a9
Harok Choi <gkfhrwkd23@naver.com>
2023-05-04 12:33:16 +0900

    test 3

--- src/main/java/com/travelproject/travelproject/TravelprojectApplication.java
+++ src/main/java/com/travelproject/travelproject/TravelprojectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TravelprojectApplication {
 
 	// 주석 aaaaa
+	// aaaaa
 	public static void main(String[] args) {
 		SpringApplication.run(TravelprojectApplication.class, args);
 	}
