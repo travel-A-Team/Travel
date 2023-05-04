9d94b6af050b913ff467ab4e8686f12755f213d5
Harok Choi <gkfhrwkd23@naver.com>
2023-05-04 12:17:18 +0900

    first commit

---
+++ src/main/java/com/travelproject/travelproject/TravelprojectApplication.java
@@ -0,0 +1,13 @@
+package com.travelproject.travelproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travelproject/travelproject/TravelprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelproject.travelproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
