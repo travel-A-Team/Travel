1d00a5ad049265c946aa795d6cc2cfa57b513b4a
jhe2426 <jhe2426@naver.com>
2023-05-10 11:02:45 +0900

    23.05.10 controller, service 폴더 안에 admin에 관한 java파일 생성 후 저장

---
+++ src/main/java/com/travelproject/travelproject/controller/adminController/adminMainController.java
@@ -0,0 +1,8 @@
+package com.travelproject.travelproject.controller.adminController;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class adminMainController {
+    
+}
---
+++ src/main/java/com/travelproject/travelproject/service/Implement/adminMainServiceImplement.java
@@ -0,0 +1,7 @@
+package com.travelproject.travelproject.service.Implement;
+
+import com.travelproject.travelproject.service.adminMainService;
+
+public class adminMainServiceImplement implements adminMainService {
+    
+}
