bae87422c4bf6eff903a7fb0c5833b6909cc651d
jhe2426 <jhe2426@naver.com>
2023-05-16 10:32:09 +0900

    2023.05.16 10:31 TouristSpotController.java 파일 이름 오타 수정 후 저장

---
+++ src/main/java/com/travelproject/travelproject/controller/admin/RecommendationTouristSpotController.java
@@ -0,0 +1,8 @@
+package com.travelproject.travelproject.controller.admin;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecommendationTouristSpotController {
+    
+}
